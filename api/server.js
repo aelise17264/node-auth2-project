2bc1ff3cecdfb42e09c759dde4732d52c0edd7d6
Anna Brander <aelise17264@gmail.com>
2020-10-13 13:38:49 -0600

    set up complete, going to test routes in postman

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const authRouter = require('../auth/auth-router')
+const usersRouter = require('../users/users-router')
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "here we go with the project for today" });
